bbb68a3af136400b0ccfa40c1c04364b353188c6
Shiv Ghyar <136742691+shiv-1540@users.noreply.github.com>
2025-04-03 12:50:57 -0700

    Update app.js

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ const server = http.createServer(app);
 const {Server} = require('socket.io');
 const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: "https://code-share-platform.vercel.app",
       methods: ["GET", "POST","PUT"],
     },
   });
